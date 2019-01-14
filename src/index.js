96d1b62513aab35cdd58536e5a8f0719552e6c38
Pau-za <molo_pau@hotmail.com>
2019-01-13 19:54:54 -0600

    Creé input para offset, botones code y decode

--- src/index.js
+++ src/index.js
@@ -7,16 +7,41 @@
 const divIntro = document.getElementById('intro');
 const divExplain = document.getElementById('cifCes');
 
-document.getElementById('botonCesar').addEventListener('click', pulsar, false);
+
+document.getElementById('botonCesar').addEventListener('click', pulsar);
     function pulsar() {
         divIntro.style.display = 'none';
         divExplain.style.display = 'block';
     };
 
 //evento del botón atrás, para volver a la Intro
-document.getElementById('backIntro').addEventListener('click', pulsarIn, false);
+document.getElementById('backIntro').addEventListener('click', pulsarIn);
     function pulsarIn() {
         divExplain.style.display = 'none';
         divIntro.style.display = 'block';
     };
+
+//evento de botón "estoy lista para iniciar"
+document.getElementById('start').addEventListener('click', pulsarStart);
+    function pulsarStart() {
+        divIntro.style.display = 'none';
+        divDesp.style.display = 'block';
+    };
+
+//Evento del botón "volver a introducción"
+//No me sale este evento :(
+const divDesp = document.getElementById('offset');
+
+document.getElementById('backIntro').addEventListener('click', clickBack);
+    function clickBack() {
+        divDesp.style.display = 'none';
+        divIntro.style.display = 'block';
+    };
+    
+//Input que recibe el número de desplazamiento
+let offset = document.getElementById('offsetNumb').value;
+console.log(offset);
+
+//Evento para cambiar de página a que usuaria elija entre codificar y decodificar mensaje
+
 
