e6fd5a2bb4252a6ffa462223834a4df5f7539c1a
Pau-za <molo_pau@hotmail.com>
2019-01-16 14:49:57 -0600

    Ya corre la función en cipher. Corregí todos los errores del html, y de los js. Hay que checar que no se haya roto el flujo de la página

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,23 @@
+
 window.cipher = {
-  encode: () => {
+  encode: (offset, userStr) => { 
+    let nuevoCifrado = ""; //variable que va a recoger el resultado de la transformación
+    //offset = document.getElementById('offsetNumb').value;
+    offset = Number(offset); //Si pongo console.log sí limpia el input, y guarda el valor
+    //userStr = document.getElementById('toEncode').value; //Hasta aquí sí agarra el string
+    let mayuscula = userStr.toUpperCase(); //Sí da el string en mayúsculas
+    for (let i = 0; i < mayuscula.length; i++){
+        let mayusAsciiOriginal = mayuscula.charCodeAt(i); //Sí da el arreglo del código ascii de cara caracter
+        let mayusAsciiTransform = (mayusAsciiOriginal - 65 + offset) %26 + 65; //si no meto el offset, todo bien
+        let transformedString = String.fromCharCode(mayusAsciiTransform);
+        nuevoCifrado += transformedString; //listo!
+    }
+    return nuevoCifrado;
   },
   decode: () => {
   },
 };
+
+/*let asciiOriginal = mayuscula.charCodeAt(); //variable que guarda el código ascii de cada caracter
+    let clearInputNumber = document.getElementsByClassName('inputCode').value=''; //limpia input
+    */
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 //Variables globales
 const divIntro = document.getElementById('intro');
 const divExplain = document.getElementById('cifCes');
-const divDesp = document.getElementById('offset');
+const divDesp = document.getElementById('choose');
 const divCode = document.getElementById('codeStr');
 const divDecode = document.getElementById('decodeStr');
 
@@ -13,7 +13,7 @@ document.getElementById('botonCesar').addEventListener('click', pulsar);
     function pulsar() {
         divIntro.style.display = 'none';
         divExplain.style.display = 'block';
-    };
+    }
 
 
 //evento de botón "estoy lista para iniciar"
@@ -21,54 +21,67 @@ document.getElementById('start').addEventListener('click', pulsarStart);
     function pulsarStart() {
         divIntro.style.display = 'none';
         divDesp.style.display = 'block';
-    };
+    }
 
 //Evento del botón "volver a introducción"
 document.getElementById('backIntro').addEventListener('click', clickBack);
     function clickBack() {
         divExplain.style.display = 'none';
         divIntro.style.display = 'block';
-    };
+    }
 
     
 //Input que recibe el número de desplazamiento al dar click en botón ingresar número
-const getOffset = () => {
+/*const getOffset = () => {
     const offset = document.getElementById('offsetNumb').value;
     console.log(Number(offset)); //Si pongo console.log sí limpia el input, y guarda el valor
     let clearInput = document.getElementById('offsetNumb').value=''; //limpia input
     return offset;
 };
 document.getElementById('offsetButton').addEventListener('click', getOffset);
+*/
 
 //Evento para insertar codificar
 document.getElementById('code').addEventListener('click', showCode);
     function showCode () {
         divCode.style.display = 'block';
-        if(divDecode.style.display = 'block'){
+        if(divDecode.style.display == 'block'){
             divDecode.style.display = 'none';
         }
-    };
+    }
 
 //Agarrar string que introduce usuario
-let nuevoCifrado = ''; //variable que va a recoger el resultado de la transformación
-const getString = () => {
-    const userStr = document.getElementById('toEncode').value; //Hasta aquí sí agarra el string
+/*let nuevoCifrado = ''; //variable que va a recoger el resultado de la transformación
+const getString = (offset, userStr) => {
+    offset = document.getElementById('offsetNumb').value;
+    offset = Number(offset); //Si pongo console.log sí limpia el input, y guarda el valor
+    userStr = document.getElementById('toEncode').value; //Hasta aquí sí agarra el string
     let mayuscula = userStr.toUpperCase(); //Sí da el string en mayúsculas
     let asciiOriginal = mayuscula.charCodeAt(); //variable que guarda el código ascii de cada caracter
+    let clearInput = document.getElementsByClassName('inputCode').value=''; //limpia input
     for (let i = 0; i < mayuscula.length; i++){
         let mayusAsciiOriginal = mayuscula.charCodeAt(i); //Sí da el arreglo del código ascii de cara caracter
-        let mayusAsciiTransform = (mayusAsciiOriginal + 65 - 9) %26 + 65; //si no meto el offset, todo bien
+        let mayusAsciiTransform = (mayusAsciiOriginal + 65 - offset) %26 + 65; //si no meto el offset, todo bien
         let transformedString = String.fromCharCode(mayusAsciiTransform);
         nuevoCifrado += transformedString; //listo!
     }
-    console.log(nuevoCifrado);
+    return nuevoCifrado;
 }
+
+//Invocando función
+document.getElementById('toCode').addEventListener('click', getString);
+console.log(getString());
+*/
+//Meto el resultado en el p con id result
+//document.getElementById('result').innerHTML='Tu mensaje cifrado es '+ nuevoCifrado;
+
+
 //NO RESPETA ESPACIOS, ¿CÓMO LE HAGO PARA INVOCAR LA FUNCIÓN'??? :'(
 
 
 
-//Invocando función
-document.getElementById('toCode').addEventListener('click', getString);
+
+
 
 
 /*
@@ -79,20 +92,15 @@ codify.addEventListener('click', () =>{
     // let cifrado = getMsj(input1,input2)
 //});
 */
-
-
-
-
-
 //Evento para insertar decodificar
 
 document.getElementById('decode').addEventListener('click', showDecode);
     function showDecode () {
         divDecode.style.display = 'block';
-        if(divCode.style.display = 'block'){
+        if(divCode.style.display == 'block'){ //tengo que checar si funciona en el evento
             divCode.style.display = 'none';
         }
-    };
+    }
 
 
     //evento del botón atrás, para volver a la Intro
@@ -103,6 +111,6 @@ function pulsarBackButton() {
     divCode.style.display = 'none';
     divDecode.style.display = 'none';
     divIntro.style.display = 'block';
-};
+}
 
 //Comencemos con el código de la función
