2f92b60dd232832353578399acaa11ff7caadd4f
Pau-za <molo_pau@hotmail.com>
2019-01-19 15:13:44 -0600

    agregué imagen, estilo

--- src/cipher.js
+++ src/cipher.js
@@ -1,27 +1,25 @@
-
 window.cipher = {
-  encode: (offset, userStr) => { 
+  encode: (offset, userStr) => {
     let nuevoCifrado = ""; //variable que va a recoger el resultado de la transformación
-     offset = Number(offset); //Si pongo console.log sí limpia el input, y guarda el valor
+    offset = Number(offset); //Si pongo console.log sí limpia el input, y guarda el valor
     let mayuscula = userStr.toUpperCase(); //Sí da el string en mayúsculas
-    for (let i = 0; i < mayuscula.length; i++){
-        let mayusAsciiOriginal = mayuscula.charCodeAt(i); //Sí da el arreglo del código ascii de cara caracter
-        let mayusAsciiTransform = (mayusAsciiOriginal - 65 + offset) %26 + 65; //si no meto el offset, todo bien
-        let transformedString = String.fromCharCode(mayusAsciiTransform);
-        nuevoCifrado += transformedString; //listo!
-      }
+    for (let i = 0; i < mayuscula.length; i++) {
+      let mayusAsciiOriginal = mayuscula.charCodeAt(i); //Sí da el arreglo del código ascii de cara caracter
+      let mayusAsciiTransform = (mayusAsciiOriginal - 65 + offset) % 26 + 65; //si no meto el offset, todo bien
+      let transformedString = String.fromCharCode(mayusAsciiTransform);
+      nuevoCifrado += transformedString; //listo!
+    }
     return nuevoCifrado;
   },
   decode: (offset, userStr) => {
     let nuevoDescifrado = "";
     let mayusculaDec = userStr.toUpperCase();
-    for(let i = 0; i < mayusculaDec.length; i++){
+    for (let i = 0; i < mayusculaDec.length; i++) {
       let mayusAsciiDec = mayusculaDec.charCodeAt(i);
-      let mayusAsciiDecTransform = (mayusAsciiDec + 65 - offset) %26 + 65;
+      let mayusAsciiDecTransform = (mayusAsciiDec + 65 - offset) % 26 + 65;
       let transformedDecString = String.fromCharCode(mayusAsciiDecTransform);
       nuevoDescifrado += transformedDecString;
     }
     return nuevoDescifrado;
   },
 };
-
--- src/index.js
+++ src/index.js
@@ -10,78 +10,81 @@ const divResultMessage = document.getElementById('resultMessage'); //div result
 
 //evento del botón para la explicación del Cifrado César
 document.getElementById('botonCesar').addEventListener('click', () => {
-    divIntro.style.display = 'none';
-    divExplain.style.display = 'block';
+  divIntro.style.display = 'none';
+  divExplain.style.display = 'block';
 })
 
 //evento de botón "estoy lista para iniciar"
 document.getElementById('start').addEventListener('click', () => {
-    divIntro.style.display = 'none';
-    divChoose.style.display = 'block';
+  divIntro.style.display = 'none';
+  divChoose.style.display = 'block';
 })
 
 //Evento del botón "volver a introducción"
 document.getElementById('backIntro').addEventListener('click', () => {
-    divExplain.style.display = 'none';
-    divIntro.style.display = 'block';
+  divExplain.style.display = 'none';
+  divIntro.style.display = 'block';
 })
 
 //Evento para codificar
 document.getElementById('code').addEventListener('click', () => {
-        divCode.style.display = 'block';
-        divResult.style.display = 'none';
-        divResultMessage.style.display = 'none';
-        if(divDecode.style.display == 'block'){
-        divDecode.style.display = 'none';
-        
-    }
+  divCode.style.display = 'block';
+  divResult.style.display = 'none';
+  divResultMessage.style.display = 'none';
+  if (divDecode.style.display == 'block') {
+    divDecode.style.display = 'none';
+
+  }
 })
 
 //Evento para decodificar
 document.getElementById('decode').addEventListener('click', () => {
-        divDecode.style.display = 'block';
-        divResult.style.display = 'none';
-        divResultMessage.style.display = 'none';
-        if(divCode.style.display == 'block'){ //tengo que checar si funciona en el evento
-        divCode.style.display = 'none';
-    }
+  divDecode.style.display = 'block';
+  divResult.style.display = 'none';
+  if (divCode.style.display == 'block') { //tengo que checar si funciona en el evento
+    divCode.style.display = 'none';
+  }
+  if (divResultMessage.style.display = 'block')
+    divResultMessage.style.display = 'none';
 })
 
 //evento del botón para volver a la Intro
 document.getElementById('backToStart').addEventListener('click', pulsarBackButton);
+
 function pulsarBackButton() {
-    divChoose.style.display = 'none';
-    divCode.style.display = 'none';
-    divDecode.style.display = 'none';
-    divResult.style.display = 'none';
-    divIntro.style.display = 'block';
+  divChoose.style.display = 'none';
+  divCode.style.display = 'none';
+  divDecode.style.display = 'none';
+  divResult.style.display = 'none';
+  divResultMessage.style.display = 'none';
+  divIntro.style.display = 'block';
 }
 
 //Agarrar el offset que introduce usuario
 document.getElementById('toCode').addEventListener('click', () => {
-    divResult.style.display = 'block';
-    divResultMessage.style.display = 'block';
-    let offset = document.getElementById('offsetEncode').value;
-    offset = Number(offset);
-    let userString = document.getElementById('inputOffset').value;
-    let result = window.cipher.encode(offset, userString);
-    return document.getElementById('result').innerHTML='Tu mensaje cifrado es '+ result + ', con un desplazamiento de ' + offset + ' lugares.';
+  divResult.style.display = 'block';
+  divResultMessage.style.display = 'block';
+  let offset = document.getElementById('offsetEncode').value;
+  offset = Number(offset);
+  let userString = document.getElementById('inputOffset').value;
+  let result = window.cipher.encode(offset, userString);
+  return document.getElementById('result').innerHTML = 'Tu mensaje cifrado es "' + result + '", con un desplazamiento de ' + offset + ' lugares.';
 })
 
 // invocando a la función decode con evento en botón to decode
 document.getElementById('toDecode').addEventListener('click', () => {
-    divResult.style.display = 'block';
-    divResultMessage.style.display = 'block';
-    let offset = document.getElementById('offsetDecode').value;
-    offset = Number(offset);
-    let userString = document.getElementById('decodeText').value;
-    let result = window.cipher.decode(offset, userString);
-    return document.getElementById('result').innerHTML='Tu mensaje cifrado es: '+ result + ', con un desplazamiento de ' + offset + ' lugares.';
+  divResult.style.display = 'block';
+  divResultMessage.style.display = 'none';
+  let offset = document.getElementById('offsetDecode').value;
+  offset = Number(offset);
+  let userString = document.getElementById('decodeText').value;
+  let result = window.cipher.decode(offset, userString);
+  return document.getElementById('result').innerHTML = 'Tu mensaje cifrado es: "' + result + '", con un desplazamiento de ' + offset + ' lugares.';
 })
 
 //Botón para limpiar formulario
-const clearInfo = document.getElementById('clearInfo').addEventListener('click', () =>{
-    location.reload();
-    divIntro.style.display = 'none'; 
-    divChoose.style.display = 'block';
+document.getElementById('clearInfo').addEventListener('click', () => {
+  location.reload();
+  divIntro.style.display = 'none';
+  divChoose.style.display = 'block';
 })
